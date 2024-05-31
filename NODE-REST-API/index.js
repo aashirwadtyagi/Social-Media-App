fab382d7859f28a738b097622df18d859c39c26a
Aashirwad Tyagi <aashirwadtyagi940@gmail.com>
2024-06-01 01:20:35 +0530

    first commit

---
+++ NODE-REST-API/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const route = require('./routes/users')
+const auth = require('./routes/auth')
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDB Connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+//Middlewares
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", route)
+app.use("/api/auth", auth)
+
+app.get("/", (req,res)=>{
+    res.send("welcome to homepage")
+})
+app.get("/users", (req,res)=>{
+    res.send("welcome to userpage")
+})
+
+
+app.listen(8800, () => { 
+    console.log(`Server is running on port ${8800}`); 
+})
+
+
+
+module.exports = app;
---
+++ NODE-REST-API/routes/auth.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const userModel = require('../models/userModel.js');
+const User = require('../models/userModel.js');
+
+router.post('/register', async (req, res) => {
+    // const user = await new userModel({
+    //     userName: "Aashirwaddd",
+    //     email: "aashirwadtyagi@gmail.comdd",
+    //     password: "abcddd"
+    // })
+    // await user.save();
+    // res.send("ok")
+    const newUser = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const user = await newUser.save();
+        res.status(200).json 
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+module.exports = router;
