110bf38ba5028de943d9efbe6afde43de585e876
Aashirwad Tyagi <aashirwadtyagi940@gmail.com>
2024-06-03 15:25:33 +0530

    commit

---
+++ frontend/src/components/topbar/Topbar.js
@@ -0,0 +1,11 @@
+
+
+function Topbar() {
+  return (
+    <div>
+      toppppbar
+    </div>
+  )
+}
+
+export default Topbar
---
+++ frontend/src/pages/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+
+function home() {
+  return (
+    <Topbar/>
+  )
+}
+
+export default home
+
